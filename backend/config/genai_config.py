f91effe5d95959d31e64f9ee020e865c3b71bab5
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-24 19:22:49 -0400

    add captions and conversion done

--- backend/config/genai_config.py
+++ backend/config/genai_config.py
@@ -1,3 +1,4 @@
+# this take in the wav file
 def process_audio(song):
     from pydub import AudioSegment
     from openai import OpenAI
@@ -10,14 +11,28 @@ def process_audio(song):
 
     first_10_minutes.export("good_morning_10.wav", format="wav")
 
+  # Export the first 10 minutes to a new file
+    temp_wav_path = "processed_10_minutes.wav"
+    first_10_minutes.export(temp_wav_path, format="wav")
+
     client = OpenAI()
 
-    audio_file = open("speech.wav", "rb")
-    transcript = client.audio.transcriptions.create(
-        file=audio_file,
-        model="whisper-1",
-        response_format="verbose_json",
-        timestamp_granularities=["word"]
+# Transcribe the processed file
+# opening the file
+    with open(temp_wav_path, "rb") as audio_file:
+        # creating the transcription
+        transcript = client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-1",
+            response_format="verbose_json",
+            timestamp_granularities=["word"]
     )
+        
+    # make the words object look like 
 
+    # give example
+
+    
+        
+    print("Transcript: ", transcript)
     return transcript.words
