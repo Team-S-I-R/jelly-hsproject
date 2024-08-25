2163773ae685db61f61ad56a34bbb7d2751528ad
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-08-25 14:54:09 -0400

    fix: BACKEND WORKING (hosted)!

--- backend/app.py
+++ backend/app.py
@@ -25,9 +25,9 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app, origins=[
-        "http://localhost:3000", 
-    "http://localhost:3001", 
-    "http://localhost:3002", 
+        "http://localhost:3000",
+        "http://localhost:3002",
+        "http://localhost:3001",
 ])
 nltk.download('vader_lexicon')
 sid = SentimentIntensityAnalyzer()
