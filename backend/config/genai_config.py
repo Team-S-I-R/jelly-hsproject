cfc98f4afebc727c78252a5bb3f08998d075cf6a
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-08-24 20:46:25 -0400

    refactor: genai_config.py

--- backend/config/genai_config.py
+++ backend/config/genai_config.py
@@ -1,8 +1,8 @@
 import os
+
 from pydub import AudioSegment
 from openai import OpenAI
 from dotenv import load_dotenv
-from config.log import logger as log
 
 load_dotenv(dotenv_path=".env")
 
@@ -12,20 +12,18 @@ def process_audio(song):
 
     ten_minutes = 10 * 60 * 1000
     first_10_minutes = song[:ten_minutes]
-    temp_wav_path = "processed_10_minutes.wav"
-    first_10_minutes.export(temp_wav_path, format="wav")
-    
+    first_10_minutes.export("good_morning_10.wav", format="wav")
+
     client = OpenAI(
         api_key=os.getenv("OPENAI_API_KEY"),
     )
 
-    with open(temp_wav_path, "rb") as audio_file:
-        transcript = client.audio.transcriptions.create(
-            file=audio_file,
-            model="whisper-1",
-            response_format="verbose_json",
-            timestamp_granularities=["word"]
+    audio_file = open("speech.wav", "rb")
+    transcript = client.audio.transcriptions.create(
+        file=audio_file,
+        model="whisper-1",
+        response_format="verbose_json",
+        timestamp_granularities=["word"]
     )
-    
-    log.info(f"Transcript: {transcript}")
+
     return transcript.words
